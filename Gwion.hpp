7d17dad943256609bc9e591f3135f7c3cae92303
fennecdjay <astor.jeremie@wanadoo.fr>
2017-03-12 02:27:41 +0100

    Fix cpp compilation.

--- Gwion.hpp
+++ Gwion.hpp
@@ -2,8 +2,62 @@ extern "C"
 {
 #include "defs.h"
 #include "err_msg.h"
-typedef _Complex complex;
+//typedef _Complex complex;
 extern m_int o_string_data;
+#ifndef __ARRAY
+#define __ARRAY
+#include <complex.h>
+#include "map.h"
+
+typedef struct VM_Vector_* VM_Vector;
+typedef struct {
+  m_uint length;
+  m_uint depth;
+  Type type;
+  m_bool is_obj;
+  m_bool is_ref;
+  m_uint stack_offset;
+} VM_Array_Info;
+
+typedef struct {
+  char*  ptr;   // data
+  m_uint len;   // number of elements * size
+  m_uint size;  // size of objects
+} M_Vector;
+
+struct VM_Array_ {
+  M_Vector* vector;
+//  Map    map;
+  Type type;
+  m_bool is_obj;
+};
+
+m_uint    m_vector_size(M_Vector* v);
+m_uint    i_vector_at  (M_Vector* v, m_uint i);
+m_float   f_vector_at  (M_Vector* v, m_uint i);
+/*
+#ifdef USE_DOUBLE
+complex double  c_vector_at  (M_Vector* v, m_uint i);
+#else
+complex float  c_vector_at  (M_Vector* v, m_uint i);
+#endif
+*/
+VEC3_T   v3_vector_at  (M_Vector* v, m_uint i);
+VEC4_T   v4_vector_at  (M_Vector* v, m_uint i);
+m_uint*   i_vector_addr(M_Vector* v, m_uint i);
+m_float*  f_vector_addr(M_Vector* v, m_uint i);
+
+_Complex c_vector_addr(M_Vector* v, m_uint i);
+VEC3_T*  v3_vector_addr(M_Vector* v, m_uint i);
+VEC4_T*  v4_vector_addr(M_Vector* v, m_uint i);
+void      i_vector_set (M_Vector* v, m_uint i, m_uint  data);
+void      f_vector_set (M_Vector* v, m_uint i, m_float data);
+void      c_vector_set (M_Vector* v, m_uint i, _Complex data);
+void     v3_vector_set (M_Vector* v, m_uint i, VEC3_T  data);
+void     v4_vector_set (M_Vector* v, m_uint i, VEC4_T  data);
+
+M_Object new_M_Array  (m_uint size, m_uint length);
+#endif
 
 #include "type.h"
 #include "dl.h"
@@ -15,6 +69,8 @@ extern m_bool add_global_type(Env env, Type t);
 m_int import_mvar(Env env, const m_str type,
   const m_str name, m_bool is_const, m_bool is_ref, const m_str doc );
 extern Func import_mfun(Env env, DL_Func * mfun);
+
+
 }
 struct Env_
 {
