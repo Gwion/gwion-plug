7df6a56d230b0d236d9370d002fd282c74c0de4a
Jérémie Astor <astor.jeremie@wanadoo.fr>
2020-01-08 12:23:13 +0100

    :art: Fix lo

--- lo/lo.c
+++ lo/lo.c
@@ -13,20 +13,19 @@
 #include "import.h"
 
 static Map map = NULL;
-struct Arg
-{
+struct Arg {
   union
   {
     m_uint  i;
     m_float f;
     m_str   s;
   } data;
-  char t;
   m_uint ref;
+  MUTEX_TYPE mutex;
+  char t;
 };
 
-struct Method
-{
+struct Method {
   m_uint ref;
   m_str path, type;
   Vector client;
@@ -34,33 +33,35 @@ struct Method
   MemPool p;
 };
 
-struct Server
-{
+struct Server {
   m_uint port, ref;
   lo_server_thread thread;
   Vector method;
   MemPool p;
 };
 
-void release_Arg(struct Arg* arg)
-{
-  if(!--arg->ref)
+void release_Arg(struct Arg* arg) {
+  MUTEX_LOCK(arg->mutex);
+  if(!--arg->ref) {
+    if(arg->t == 's')
+      free(arg->data.s);
+    MUTEX_UNLOCK(arg->mutex);
+    MUTEX_CLEANUP(arg->mutex);
     free(arg);
+  } else
+    MUTEX_UNLOCK(arg->mutex);
 }
-void release_method(struct Server* s, struct Method* m)
-{
-  if(!--m->ref)
-  {
+
+void release_method(struct Server* s, struct Method* m) {
+  if(!--m->ref) {
     lo_server_thread_del_method(s->thread, m->path, m->type);
     vector_rem(s->method, vector_find(s->method, (vtype)m));
     free(m);
   }
 }
 
-void release_server(struct Server* s)
-{
-  if(!--s->ref)
-  {
+void release_server(struct Server* s) {
+  if(!--s->ref) {
     map_remove(map, (vtype)s->port);
     lo_server_thread_stop(s->thread);
     lo_server_thread_free(s->thread);
@@ -69,13 +70,11 @@ void release_server(struct Server* s)
   }
 }
 
-struct Arg* new_Arg(char t, char* stack)
-{
+struct Arg* new_Arg(char t, char* stack) {
   struct Arg* arg = (struct Arg*)xcalloc(1, sizeof(struct Arg));
   arg->t = t;
   arg->ref = 1;
-  switch(t)
-  {
+  switch(t) {
     case 'i':
       arg->data.i = *(m_int*)  (stack - SZ_INT);
       break;
@@ -89,37 +88,34 @@ struct Arg* new_Arg(char t, char* stack)
   return arg;
 }
 
-static m_int o_lo_addr, o_lo_args, o_lo_serv, o_lo_meth, o_lo_curr;
+static m_int o_lo_addr, o_lo_args, o_lo_serv, o_lo_meth, o_lo_curr, o_lo_mutex;
 #define ADDR(o) *(lo_address*)    (o->data + o_lo_addr)
 #define ARGS(o) *(Vector*)        (o->data + o_lo_args)
 #define SERV(o) *(struct Server**)(o->data + o_lo_serv)
 #define METH(o) *(Vector*)        (o->data + o_lo_meth)
 #define CURR(o) *(Vector*)     (o->data + o_lo_curr)
+#define CURR(o) *(Vector*)     (o->data + o_lo_curr)
+#define MUTEX(o) *(MUTEX_TYPE*)     (o->data + o_lo_mutex)
 
-static MFUN(osc_addr)
-{
+static MFUN(osc_addr) {
   if(ADDR(o))
     lo_address_free(ADDR(o));
-  m_str host = STRING(*(M_Object*)MEM(SZ_INT)); 
+  m_str host = STRING(*(M_Object*)MEM(SZ_INT));
   m_str port = STRING(*(M_Object*)MEM(SZ_INT * 2));
   *(m_uint*)RETURN = (ADDR(o) = lo_address_new(host, port)) ? 1 : 0;
 }
 
-static MFUN(osc_send)
-{
-  m_uint i;
-  struct Arg* arg;
-  lo_message msg;
+static MFUN(osc_send) {
   if(!ADDR(o))
   {
     gw_err("oscsend address not set. shred[%i] exiting.", shred->tick->xid);
     vm_shred_exit(shred);
     return;
   }
-  msg = lo_message_new();
-  for(i = 0; i < vector_size(ARGS(o)); i++)
+  lo_message msg = lo_message_new();
+  for(m_uint i = 0; i < vector_size(ARGS(o)); i++)
   {
-    arg = (struct Arg*)vector_at(ARGS(o), i);
+    struct Arg *arg = (struct Arg*)vector_at(ARGS(o), i);
     switch(arg->t)
     {
       case 'i':
@@ -164,20 +160,14 @@ static void osc_error_handler(int num, const char *msg, const char *where)
 { gw_err("problem %i with osc: %s %s", num, msg, where); }
 
 static int osc_method_handler(const char* path, const char* type,
-  lo_arg **argv, int argc, lo_message msg, void *data)
-{
-  m_uint i, j;
+  lo_arg **argv, int argc, lo_message msg, void *data) {
   struct Method* m = (struct Method*)data;
-  M_Object o;
-  Vector v;
-
-  v = new_vector(m->p);
-  for(i = 0; i < argc; i++)
-  {
-    struct Arg* arg = (struct Arg*)xmalloc(sizeof(struct Arg));
+  const Vector v = new_vector(m->p);
+  for(m_uint i = 0; i < argc; i++) {
+    struct Arg* arg = (struct Arg*)xcalloc(1, sizeof(struct Arg));
     arg->ref = 1;
-    switch(type[i])
-    {
+    MUTEX_SETUP(arg->mutex);
+    switch(type[i]) {
       case 'i':
         arg->data.i = argv[i]->i;
         arg->t = 'i';
@@ -195,33 +185,43 @@ static int osc_method_handler(const char* path, const char* type,
         break;
       default:
         gw_err("unhandled osc arg type '%c'", type[i]);
+// leaks vector, at least
         return GW_OK;
     }
     arg->t = type[i];
     vector_add(v, (vtype)arg);
   }
-  for(i = 0; i < vector_size(m->client); i++)
-  {
-    for(j = 0; j < argc; j++)
-      ((struct Arg*)vector_at(v, j))->ref++;
-    o = (M_Object)vector_at(m->client, i);
+  for(m_uint i = 0; i < vector_size(m->client); i++) {
+    for(m_uint j = 0; j < argc; j++) {
+      struct Arg* arg = (struct Arg*)vector_at(v, j);
+      arg->ref++;
+      MUTEX_LOCK(arg->mutex);
+    }
+    const M_Object o = (M_Object)vector_at(m->client, i);
+    MUTEX_LOCK(MUTEX(o));
+    const Vector vec = vector_copy(m->p, v);
+    vector_add(ARGS(o), (vtype)vec);
     broadcast(o);
-    vector_add(ARGS(o), (vtype)vector_copy(m->p, v));
+    MUTEX_UNLOCK(MUTEX(o));
+    for(m_uint j = 0; j < argc; j++) {
+      struct Arg* arg = (struct Arg*)vector_at(v, j);
+      MUTEX_UNLOCK(arg->mutex);
+    }
   }
-  for(i = 0; i < argc; i++)
-    release_Arg((struct Arg*)vector_at(v, i));
+  for(m_uint j = 0; j < argc; j++) {
+    struct Arg* arg = (struct Arg*)vector_at(v, j);
+    release_Arg(arg);
+  }
   free_vector(m->p, v);
   return 0;
 }
 
-static MFUN(osc_get_port)
-{
+static MFUN(osc_get_port) {
   struct Server* s = SERV(o);
   *(m_uint*)RETURN = s ? s->port : -1;
 }
 
-static MFUN(osc_port)
-{
+static MFUN(osc_port) {
   struct Server* s = NULL;
   /*if(!o)*/
   /*{*/
@@ -258,8 +258,7 @@ static MFUN(osc_port)
   *(m_uint*)RETURN = s->port = port;
 }
 
-static MFUN(osc_add_method)
-{
+static MFUN(osc_add_method) {
   m_uint i;
   struct Method* m = NULL;
   struct Server* s = SERV(o);
@@ -293,22 +292,20 @@ found:
 }
 
 static CTOR(lo_ctor){ ARGS(o) = new_vector(shred->info->mp); }
-static CTOR(loin_ctor){ METH(o) = new_vector(shred->info->mp); CURR(o) = new_vector(shred->info->mp);}
+static CTOR(loin_ctor){ METH(o) = new_vector(shred->info->mp); CURR(o) = new_vector(shred->info->mp); MUTEX_SETUP(MUTEX(o)); }
 static CTOR(lo_dtor) { free_vector(shred->info->mp, ARGS(o)); }
 static CTOR(loout_dtor){ if(ADDR(o))lo_address_free(ADDR(o));}
-static CTOR(loin_dtor){ free_vector(shred->info->mp, METH(o)); free_vector(shred->info->mp, CURR(o));}
+static CTOR(loin_dtor){ free_vector(shred->info->mp, METH(o)); free_vector(shred->info->mp, CURR(o)); MUTEX_CLEANUP(MUTEX(o)); }
 
-static MFUN(oscin_stop)
-{
+static MFUN(oscin_stop) {
   struct Server* s = SERV(o);
   *(m_uint*)RETURN = lo_server_thread_stop(s->thread);
 }
 
-static MFUN(oscin_start)
-{
+static MFUN(oscin_start) {
   struct Server* s = SERV(o);
-  if(lo_server_thread_start(s->thread) < 0)
-  {
+  const int ret = lo_server_thread_start(s->thread);
+  if(ret < 0) {
     release_server(s);
     *(m_uint*)RETURN = -1;
     return;
@@ -316,29 +313,35 @@ static MFUN(oscin_start)
   *(m_uint*)RETURN = 1;
 }
 
-static MFUN(osc_recv)
-{
+static MFUN(osc_recv) {
+  MUTEX_LOCK(MUTEX(o));
   Vector c_arg = ARGS(o);
+  if(!c_arg) {
+    *(m_uint*)RETURN = 0;
+    MUTEX_UNLOCK(MUTEX(o));
+    return;
+  }
+  if(!vector_size(c_arg)) {
+    *(m_uint*)RETURN = 0;
+    MUTEX_UNLOCK(MUTEX(o));
+    return;
+  }
   CURR(o) = (Vector)vector_at(c_arg, 0);
-  *(m_uint*)RETURN = CURR(o) ? 1 : 0;
+  *(m_uint*)RETURN = !!vector_size(CURR(o));
   vector_rem(ARGS(o), 0);
+  MUTEX_UNLOCK(MUTEX(o));
 }
-static MFUN(oscin_rem)
-{
-  m_uint i;
-  struct Method* m = NULL;
-  POP_REG(shred, SZ_INT*2);
+
+static MFUN(oscin_rem) {
   m_str path = STRING(*(M_Object*)MEM(0));
   m_str type = STRING(*(M_Object*)MEM(SZ_INT));
-  if(!SERV(o)) // no server
+  if(!SERV(o))
     return;
-  for(i = 0; i < vector_size(METH(o)); i++)
-  {
-    m = (struct Method*)vector_at(METH(o), i);
+  for(m_uint i = 0; i < vector_size(METH(o)); i++) {
+    struct Method *m = (struct Method*)vector_at(METH(o), i);
     if(strcmp(m->path, path))
       continue;
-    if(!strcmp(m->type, type))
-    {
+    if(!strcmp(m->type, type)) {
       if((i = vector_find(m->client, (vtype)o)) >= 0)
         vector_rem(m->client, (vtype)i);
       if((i = vector_find(METH(o), (vtype)m)) >= 0)
@@ -350,36 +353,40 @@ static MFUN(oscin_rem)
   }
 }
 
-static MFUN(oscin_get_i)
-{
+static MFUN(oscin_get_i) {
+  MUTEX_LOCK(MUTEX(o));
   Vector c_arg = CURR(o);
   struct Arg* arg = (struct Arg*)vector_at(c_arg, 0);
-  vector_rem(c_arg, 0);
   *(m_uint*)RETURN = arg->data.i;
   release_Arg(arg);
+  vector_rem(c_arg, 0);
+  MUTEX_UNLOCK(MUTEX(o));
 }
 
-static MFUN(oscin_get_f)
-{
+static MFUN(oscin_get_f) {
+  MUTEX_LOCK(MUTEX(o));
   Vector c_arg = CURR(o);
   struct Arg* arg = (struct Arg*)vector_at(c_arg, 0);
-  vector_rem(c_arg, 0);
   *(m_float*)RETURN = arg->data.f;
   release_Arg(arg);
+  vector_rem(c_arg, 0);
+  MUTEX_UNLOCK(MUTEX(o));
 }
 
-static MFUN(oscin_get_s)
-{
+static MFUN(oscin_get_s) {
+  MUTEX_LOCK(MUTEX(o));
   Vector c_arg = CURR(o);
   struct Arg* arg = (struct Arg*)vector_at(c_arg, 0);
-  vector_rem(c_arg, 0);
   *(m_uint*)RETURN = (m_uint)new_string(shred->info->vm->gwion->mp, shred, arg->data.s);
   release_Arg(arg);
+  vector_rem(c_arg, 0);
+  MUTEX_UNLOCK(MUTEX(o));
 }
 
 GWION_IMPORT(lo) {
   const Type t_lo = gwi_class_ini(gwi, "Lo", "Event");
   SET_FLAG(t_lo, abstract);
+  gwi_class_xtor(gwi, lo_ctor, lo_dtor);
   gwi_item_ini(gwi, "@internal", "@args");
   o_lo_args = gwi_item_end(gwi, ae_flag_member, NULL);
   GWI_BB(o_lo_args)
@@ -410,6 +417,9 @@ GWION_IMPORT(lo) {
   gwi_item_ini(gwi, "@internal", "@curr");
   o_lo_curr = gwi_item_end(gwi, ae_flag_member, NULL);
   GWI_BB(o_lo_curr)
+  gwi_item_ini(gwi, "@internal", "@mutex");
+  o_lo_mutex = gwi_item_end(gwi, ae_flag_member, NULL);
+  GWI_BB(o_lo_mutex)
   gwi_func_ini(gwi, "int", "add");
     gwi_func_arg(gwi, "string", "path");
     gwi_func_arg(gwi, "string", "type");
@@ -429,7 +439,7 @@ GWION_IMPORT(lo) {
   gwi_func_ini(gwi, "int", "get_i");
   GWI_BB(gwi_func_end(gwi, oscin_get_i, ae_flag_none))
   gwi_func_ini(gwi, "float", "get_f");
-  GWI_BB(gwi_func_end(gwi, oscin_get_f, ae_flag_none))  
+  GWI_BB(gwi_func_end(gwi, oscin_get_f, ae_flag_none))
   gwi_func_ini(gwi, "string", "get_s");
   GWI_BB(gwi_func_end(gwi, oscin_get_s, ae_flag_none))
 
