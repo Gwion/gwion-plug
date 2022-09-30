73791d0ada6952f5e448c79bf9a4e68a20a2fa30
fennecdjay <fennecdjay@gmail.com>
2022-09-30 13:41:35 +0200

    update Lsys

--- Lsys/plug.c
+++ Lsys/plug.c
@@ -18,101 +18,78 @@
 
 #include "lsys.h"
 
-typedef struct
-{
+typedef struct {
   lsys_d       lsys;
   lsys_list    lst;
   lsys_entry * ent;
   m_uint pos;
-  m_bool is_init;
 } Lsys;
 
 #define LSYS(o) ((Lsys*)UGEN(o)->module.gen.data)
 
 static int tochar(int c) {
-    if(c >= 10) {
-        return c + 87;
-    } else {
-        return c + 48;
-    }
+  if(c >= 10) return c + 87;
+  else return c + 48;
 }
 
 static TICK(tick) {
   Lsys* ptr = (Lsys*)u->module.gen.data;
   u->out = 0;
-  if(!ptr->is_init)
-    return;
   if(u->module.gen.trig->out) {
     ptr->pos = lsys_list_iter(&ptr->lst, &ptr->ent, ptr->pos);
     u->out = ptr->ent->val + 1;
   }
 }
 
-static CTOR(ctor) {
-  Lsys* l = (Lsys*) xmalloc(sizeof(Lsys));
+static DTOR(dtor) {
+  Lsys* l = LSYS(o);
+  lsys_list_destroy(&l->lst);
+  mp_free2(shred->info->mp, sizeof(Lsys), l);
+}
+
+static MFUN(gw_lsys_parse) {
+  Lsys* l = (Lsys*) mp_malloc2(shred->info->mp, sizeof(Lsys));
   lsys_init(&l->lsys);
-  l->is_init = 0;
-  l->pos = 0;
   ugen_ini(shred->info->vm->gwion, UGEN(o), 0, 1);
   ugen_gen(shred->info->vm->gwion, UGEN(o), tick, l, 1);
-}
 
-static DTOR(dtor)
-{
-  Lsys* l = LSYS(o);
-  if(l->is_init)
-    lsys_list_destroy(&l->lst);
-  free(l);
-}
-
-static MFUN(gw_lsys_parse)
-{
-  Lsys*  ptr = LSYS(o);
   m_uint ord = *(m_uint*)MEM(SZ_INT);
   m_str  str = STRING(*(M_Object*)MEM(SZ_INT*2));
 
-  lsys_parse(&ptr->lsys, str, str, strlen(str));
-  lsys_list_init(&ptr->lst);
-  lsys_make_list(&ptr->lsys, &ptr->lst, str, 0, ord);
+  lsys_parse(&l->lsys, str, str, strlen(str));
+  lsys_list_init(&l->lst);
+  lsys_make_list(&l->lsys, &l->lst, str, 0, ord);
+  lsys_list_reset(&l->lst);
+  l->pos     = 0;
+  *(M_Object*) RETURN = o;
+}
+
+static MFUN(gw_lsys_reset) {
+  Lsys*  ptr = LSYS(o);
   lsys_list_reset(&ptr->lst);
-
-  ptr->is_init = 1;
-  ptr->pos     = 0;
 }
 
-static MFUN(gw_lsys_reset)
-{
+static MFUN(gw_lsys_size) {
   Lsys*  ptr = LSYS(o);
-  if(ptr->is_init)
-    lsys_list_reset(&ptr->lst);
-}
-
-static MFUN(gw_lsys_size)
-{
-  Lsys*  ptr = LSYS(o);
-  *(m_int*)RETURN = ptr->is_init ? (m_int)ptr->lst.size : -1;
+  *(m_int*)RETURN = ptr->lst.size;
 }
 
 static MFUN(gw_lsys_get) {
   Lsys*  ptr   = LSYS(o);
-  if(!ptr->is_init)
-  {
-    handle(shred, "LSysNotInitiated");
-    return;
-  }
-  char str[ptr->lst.size];
+  char str[ptr->lst.size + 1];
   for(uint i = 0; i < ptr->lst.size; i++) {
     ptr->pos = lsys_list_iter(&ptr->lst, &ptr->ent, ptr->pos);
     str[i] = tochar(ptr->ent->val + 1);
   }
+  str[ptr->lst.size] = '\0';
   *(m_uint*)RETURN = (m_uint)new_string(shred->info->vm->gwion, str);
 }
 
 GWION_IMPORT(lsys) {
   GWI_OB(gwi_class_ini(gwi, "Lsys", "UGen"))
-  gwi_class_xtor(gwi, ctor, dtor);
+  gwi_class_xtor(gwi, NULL, dtor);
 
-  GWI_BB(gwi_func_ini(gwi, "void", "parse"))
+  GWI_BB(gwi_func_ini(gwi, "auto", "new"))
     GWI_BB(gwi_func_arg(gwi, "int",    "ord"))
     GWI_BB(gwi_func_arg(gwi, "string", "str"))
   GWI_BB(gwi_func_end(gwi, gw_lsys_parse, ae_flag_none))
