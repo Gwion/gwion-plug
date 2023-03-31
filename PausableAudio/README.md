3410ba0416ba3d2c18305f5a0518541c8aa90626
fennecdjay <fennecdjay@gmail.com>
2023-03-31 12:29:20 +0200

    :art: Introduce PausableAudio

---
+++ PausableAudio/README.md
@@ -0,0 +1,16 @@
+# PausableAudio
+  a [Gwion](https://github.com/Gwion/Gwion) plugin.  
+## Description
+use [PausableAudio](https://github.com/.../PausableAudio)
+## Configuration
+check your [Gwion-plug](https://github.com/Gwion/gwion-plug) configuration. (e.g. edit config.mk)  
+edit Makefile
+## Building
+ensure [PausableAudio](https://github.com/.../PausableAudio) is installed
+```
+make
+# optionnal
+make install
+```
+# Usage
+check .gw files in the directory.
---
+++ PausableAudio/pausableaudio.c
@@ -0,0 +1,98 @@
+#include "gwion_util.h"
+#include "gwion_ast.h"
+#include "gwion_env.h"
+#include "vm.h"
+#include "gwion.h"
+#include "object.h"
+#include "operator.h"
+#include "instr.h"
+#include "import.h"
+
+typedef struct Pausable_ {
+  DriverData dd;
+  bool play;
+  bool suspend;
+} Pausable;
+
+static DRVRUN(pausable_run) {
+  Pausable *pausable = di->driver->data;
+  Driver drv = *di;
+  drv.driver = &pausable->dd;
+  while (di->is_running) {
+    if(!pausable->play) {
+      di->run(vm);
+      next_bbq_pos(vm);
+    } else {
+      pausable->dd.run(vm, &drv);
+      if(pausable->suspend)
+        di->is_running = true;
+      pausable->suspend = false;
+    }
+  }
+}
+
+static DRVDEL(pausable_del) {
+  Pausable *pausable = di->driver->data;
+  Driver drv = *di;
+  drv.driver = &pausable->dd;
+  if(pausable->dd.del) pausable->dd.del(vm, &drv);
+  mp_free(vm->gwion->mp, Pausable, pausable);
+}
+
+ANN static m_bool child_init(VM *vm, Driver *drv) {
+  DECL_OB(gwdriver_t, func, = driver_ini(vm->gwion, drv->si));
+  func(drv->driver);
+  return drv->driver->ini(vm, drv);
+}
+
+static DRVINI(pausable_ini) {
+  char *const old_arg = di->si->arg;
+  char *arg = strchr(old_arg, '=');
+  if(!arg) {
+    gw_err("{R}PausableAudio{0} driver needs an argument\n");
+    return GW_ERROR;
+  }
+  arg++;
+  struct SoundInfo_ si = *di->si;
+  si.arg = arg;
+  Pausable *pausable = di->driver->data =
+    mp_calloc(vm->gwion->mp, Pausable);
+  Driver drv = {
+    .driver = &pausable->dd,
+    .si = &si
+  };
+  if(child_init(vm, &drv) > 0) return GW_OK;
+  gw_err("{R}PausableAudio{0} error while initializing child driver\n");
+  return GW_ERROR;
+}
+
+GWDRIVER(PausableAudio) {
+  d->ini = pausable_ini;
+  d->run = pausable_run;
+  d->del = pausable_del;
+}
+
+static SFUN(get) {
+  Pausable *pausable = shred->info->vm->bbq->driver->data;
+  *(m_uint*)RETURN = pausable->play;
+}
+
+static SFUN(toggle) {
+  Pausable *pausable = shred->info->vm->bbq->driver->data;
+  pausable->play = *(m_uint*)MEM(0);
+  if(!pausable->play) {
+    shred->info->vm->bbq->is_running = false;
+    shredule(shred->tick->shreduler, shred, GWION_EPSILON);
+  } else pausable->suspend = true;
+}
+
+GWION_IMPORT(PausableAudio) {
+  GWI_OB(gwi_struct_ini(gwi, "PausableAudio"));
+  GWI_BB(gwi_func_ini(gwi, "bool", "play"));
+  GWI_BB(gwi_func_end(gwi, get, ae_flag_static));
+  GWI_BB(gwi_func_ini(gwi, "void", "play"));
+  GWI_BB(gwi_func_arg(gwi, "bool", "p"));
+  GWI_BB(gwi_func_end(gwi, toggle, ae_flag_static));
+  GWI_BB(gwi_class_end(gwi));
+  return GW_OK;
+}
